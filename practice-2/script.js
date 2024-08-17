15fd3e5384a507ac18a5e599ef01e66ba1e3dff5
Irina Rusinova <irina.rusinova19@gmail.com>
2024-08-17 23:09:14 +0300

    get data and show draft table

---
+++ practice-2/script.js
@@ -0,0 +1,41 @@
+const url = 'https://jsonplaceholder.typicode.com/posts';
+const main = document.querySelector('.main');
+
+const convertDataInTable = (data) => {
+  return data && data.reduce((table, current) => {
+    const columnWithCurrentUserId = table.find((column) => column.userId === current.userId);
+    if (columnWithCurrentUserId) {
+      columnWithCurrentUserId.content.push({
+        title: current.title,
+        body: current.body
+      });
+    } else {
+      const newColumn = {
+        userId: current.userId,
+        content: [{
+          title: current.title,
+          body: current.body
+        }]
+      };
+      table.push(newColumn);
+    }
+    return table;
+  }, []);
+}
+
+const generateTable = (data) => {
+  const table = document.createElement('div');
+  table.classList.add('table');
+  for (let columnData of data) {
+    const column = document.createElement('div');
+    column.classList.add('column');
+    column.innerHTML = columnData.content.map((post) => `<div class="cell"><p class="title">${post.title}</p><p class="body">${post.body}</p></div>`);
+
+    table.appendChild(column);
+  }
+  main.appendChild(table);
+}
+
+fetch(url).then((response) => response.json())
+.then((data) => convertDataInTable(data))
+.then((data) => generateTable(data));
