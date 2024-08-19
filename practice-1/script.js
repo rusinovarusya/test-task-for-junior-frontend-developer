44aef739175ac59d8b42c0fde5bbdfa40e9da385
Irina Rusinova <irina.rusinova19@gmail.com>
2024-08-19 21:18:01 +0300

    fix background

--- practice-1/script.js
+++ practice-1/script.js
@@ -1,60 +1,65 @@
-const form = document.forms[0];
-
-const inputList = document.querySelectorAll('input');
-
-const modal = document.querySelector('.modal');
-
-const submitButton = document.querySelector('.submit-button');
-
-const resetButton = document.querySelector('.reset-button');
-
-const logoInput = document.querySelector('.logo-input__input');
-
-const buttonDeleteLogo = document.querySelector('.delete-image-button__icon');
-
-const openModalButton = document.querySelector('.open-modal-button');
-
-const closeModal = () => {
-  modal.classList.add('hidden');
-}
-
-const checkForm = () => {
-  if ([...inputList].every((item) => item.checkValidity())) {
-    submitButton.disabled = false;
-  } else {
-    submitButton.disabled = true;
+(function () {
+  const form = document.forms[0];
+  const inputList = document.querySelectorAll('input');
+  const modal = document.querySelector('.modal');
+  const submitButton = document.querySelector('.submit-button');
+  const resetButton = document.querySelector('.reset-button');
+  const logoInput = document.querySelector('.logo-input__input');
+  const buttonDeleteLogo = document.querySelector('.delete-image-button__icon');
+  const openModalButton = document.querySelector('.open-modal-button');
+  const select = document.querySelector('.direction');
+  
+  const state = {};
+  
+  const closeModal = () => {
+    modal.classList.add('hidden');
   }
-}
-
-const loadImage = (e) => {
-  const files = e.target.files;
-  if (files.length) {
-    const image = files[0];
-    let src = URL.createObjectURL(image);
+  
+  const checkForm = () => {
+    if ([...inputList].every((item) => item.checkValidity())) {
+      submitButton.disabled = false;
+    } else {
+      submitButton.disabled = true;
+    }
+  }
+  
+  const loadImage = (e) => {
+    const files = e.target.files;
+    if (files.length) {
+      const image = files[0];
+      let src = URL.createObjectURL(image);
+      const previewImg = document.querySelector('.logo-input__preview-img');
+      previewImg.src = src;
+    }
+  }
+  
+  const deleteLogo = (e) => {
+    e.preventDefault();
+    logoInput.nodeValue = null;
     const previewImg = document.querySelector('.logo-input__preview-img');
-    previewImg.src = src;
+    previewImg.src = null;
   }
-}
+  
+  const openModal = () => {
+    modal.classList.remove('hidden');
+  }
+  
+  form.addEventListener('change', checkForm);
+  submitButton.addEventListener('click', closeModal);
+  resetButton.addEventListener('click', closeModal);
+  logoInput.addEventListener('change', loadImage);
+  buttonDeleteLogo.addEventListener('click', deleteLogo);
+  openModalButton.addEventListener('click', openModal);
+  
+  form.addEventListener('submit', () => {
+    inputList.forEach((element) => {
+      if (element.name) {
+        state[element.name] = element.nodeValue;
+      }
+    });
+    state.direction = select.nodeValue;
+  });
 
-const deleteLogo = (e) => {
-  e.preventDefault();
-  logoInput.nodeValue = null;
-  const previewImg = document.querySelector('.logo-input__preview-img');
-  previewImg.src = null;
-}
+})();
 
-const openModal = () => {
-  modal.classList.remove('hidden');
-}
 
-form.addEventListener('change', checkForm);
-
-submitButton.addEventListener('click', closeModal);
-
-resetButton.addEventListener('click', closeModal);
-
-logoInput.addEventListener('change', loadImage);
-
-buttonDeleteLogo.addEventListener('click', deleteLogo);
-
-openModalButton.addEventListener('click', openModal);
