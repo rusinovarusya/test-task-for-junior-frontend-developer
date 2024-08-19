cab4ff755c2b319e6542dea229d1c4732e61d5b1
Irina Rusinova <irina.rusinova19@gmail.com>
2024-08-19 17:00:17 +0300

    add button to open modal, logo management

--- practice-1/script.js
+++ practice-1/script.js
@@ -8,16 +8,43 @@ const submitButton = document.querySelector('.submit-button');
 
 const resetButton = document.querySelector('.reset-button');
 
+const logoInput = document.querySelector('.logo-input__input');
+
+const buttonDeleteLogo = document.querySelector('.delete-image-button__icon');
+
+const openModalButton = document.querySelector('.open-modal-button');
+
 const closeModal = () => {
   modal.classList.add('hidden');
 }
 
 const checkForm = () => {
-  if (inputList.every((item) => item.checkValidity())) {
+  if ([...inputList].every((item) => item.checkValidity())) {
     submitButton.disabled = false;
   } else {
     submitButton.disabled = true;
   }
+}
+
+const loadImage = (e) => {
+  const files = e.target.files;
+  if (files.length) {
+    const image = files[0];
+    let src = URL.createObjectURL(image);
+    const previewImg = document.querySelector('.logo-input__preview-img');
+    previewImg.src = src;
+  }
+}
+
+const deleteLogo = (e) => {
+  e.preventDefault();
+  logoInput.nodeValue = null;
+  const previewImg = document.querySelector('.logo-input__preview-img');
+  previewImg.src = null;
+}
+
+const openModal = () => {
+  modal.classList.remove('hidden');
 }
 
 form.addEventListener('change', checkForm);
@@ -25,3 +52,9 @@ form.addEventListener('change', checkForm);
 submitButton.addEventListener('click', closeModal);
 
 resetButton.addEventListener('click', closeModal);
+
+logoInput.addEventListener('change', loadImage);
+
+buttonDeleteLogo.addEventListener('click', deleteLogo);
+
+openModalButton.addEventListener('click', openModal);
